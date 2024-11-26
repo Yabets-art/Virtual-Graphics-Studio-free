55ee76a504b560c96dd1b323c9bff5023002fdc5
Yabets-art <yabetsd29@gmail.com>
2024-11-26 09:04:37 -0500

    home page slide gallary1

--- includes/home/class-home.php
+++ includes/home/class-home.php
@@ -7,31 +7,35 @@ function vgd_render_dashboard_tab() {
     echo my_auto_sliding_gallery();
     echo do_shortcode('[auto_sliding_gallery]');
 
-}
-function my_auto_sliding_gallery() {
-    // Fetch images dynamically (example: from a media library query or custom field)
-    $images_dir = get_template_directory_uri() . '/assets/pictures/';
-    $images = array(
-        'image1.png',
-        'image2.png',
-        'image3.png',
-    );
 
-    ob_start();
-    ?>
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <?php foreach ($images as $image): ?>
-                <div class="swiper-slide">
-                    <img src="<?php echo esc_url($image); ?>" alt="Gallery Image">
-                </div>
-            <?php endforeach; ?>
-        </div>
-        <!-- Optional navigation -->
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-    </div>
-    <?php
-    return ob_get_clean();
 }
-add_shortcode('auto_sliding_gallery', 'my_auto_sliding_gallery');
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!function_exists('my_auto_sliding_gallery')) {
+    function my_auto_sliding_gallery() {
+        $images_dir = VGD_PLUGIN_URL . 'assets/pictures/';
+        $images = ['image1.png', 'image2.png', 'image3.png'];
+        ob_start();
+        ?>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <?php foreach ($images as $image): ?>
+                    <div class="swiper-slide">
+                        <img src="<?php echo esc_url($images_dir . $image); ?>" alt="Gallery Image">
+                    </div>
+                <?php endforeach; ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+
+    // Register the shortcode
+    add_shortcode('auto_sliding_gallery', 'my_auto_sliding_gallery');
+}
